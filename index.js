86bdd4afeb8b64f793d6789bae68f1ce37ba2bc8
Tim Garthwaite <tim@garthwaite.org>
2016-03-08 11:42:37 -0500

    Debugging subrouting

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ var fs = require('fs');
 
 module.exports = function(dirname) {
   var router = express.Router({ mergeParams: true });
+  console.log('======');
+  console.log(dirname);
+  console.log('------');
   fs.readdirSync(dirname).forEach(function(file) {
+    console.log(file);
     if(file === 'index.js'
         || file.substr(file.lastIndexOf('.') + 1) !== 'js'
         || file.substr(0, 1) === '_')
       return;
-    routeName = file.substr(0, file.indexOf('.'));
-    subRouter = require(dirname + '/' + routeName);
-    router.use('/' + routeName, subRouter);
-    console.log(JSON.stringify(router.stack[router.stack.length - 1].route));
+    routeName = '/' + file.substr(0, file.lastIndexOf('.'));
+    console.log(routeName);
+    subRouter = require(dirname + routeName);
+    console.log(JSON.stringify(subRouter.stack[0].route));
+    router.use(routeName, subRouter);
+    console.log(JSON.stringify(router.stack[0].route));
   });
   return router;
 };
