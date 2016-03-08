a7a2b016e9d3eef6d2262535dfdbe0f29af58789
Tim Garthwaite <tim@garthwaite.org>
2016-03-08 10:02:44 -0500

    Replace Coffeescript syntax.  Oops.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var fs = require("fs");
 
-module.exports = (dirname) ->
+module.exports = function(dirname) {
   var router = express.Router();
   fs.readdirSync(dirname).forEach(function(file) {
     if (file === "index.js" || file.substr(file.lastIndexOf(".") + 1) !== "js" || file.substr(0, 1) === "_")
@@ -9,4 +9,5 @@ module.exports = (dirname) ->
     router.use(require("./" + file.substr(0, file.indexOf("."))));
     return router;
   });
+};
 
