253e8522155643cf560711f808c7b1bdd21372e3
Tim Garthwaite <tim@garthwaite.org>
2016-03-08 09:12:57 -0500

    Now have to pass in the dirname, since it otherwise looks in node_modules

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 Cascade Express routes in a directory hierarchy
 
+If the current directory has routes to add, as well as subdirectories with
+routes, build up your index.js as follows:
+
+    var cascadeRoutes = require('express-cascade-routes');
+    var express = require('express');
+    var router = express.Router();
+    router.post('/', function(req, res) {
+        // ...
+    });
+    router.use(cascadeRoutes(__dirname));
+    module.exports = router;
+
+If the current directory has no routes, but needs to cascade routes
+contained in subfolders, just place an index.js with the following:
+
+    module.exports = require('express-cascade-routes')(__dirname)
+
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var express = require("express");
 var fs = require("fs");
 
-var router = express.Router();
-
-fs.readdirSync(__dirname).forEach(function(file) {
-  if (file === "index.js" || file.substr(file.lastIndexOf(".") + 1) !== "js" || file.substr(0, 1) === "_")
-    return;
-  return router.use(require("./" + file.substr(0, file.indexOf("."))));
-});
-
-module.exports = router;
+module.exports = (dirname) ->
+  var router = express.Router();
+  fs.readdirSync(dirname).forEach(function(file) {
+    if (file === "index.js" || file.substr(file.lastIndexOf(".") + 1) !== "js" || file.substr(0, 1) === "_")
+      return;
+    router.use(require("./" + file.substr(0, file.indexOf("."))));
+    return router;
+  });
 
