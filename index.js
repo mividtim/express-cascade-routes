8add8a5b18bdfce1a60fd2b578273e1f11b5f381
Tim Garthwaite <tim@garthwaite.org>
2016-03-08 10:06:11 -0500

    Use dirname in require stmt

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function(dirname) {
   fs.readdirSync(dirname).forEach(function(file) {
     if (file === "index.js" || file.substr(file.lastIndexOf(".") + 1) !== "js" || file.substr(0, 1) === "_")
       return;
-    router.use(require("./" + file.substr(0, file.indexOf("."))));
+    router.use(require(dirname + "/" + file.substr(0, file.indexOf("."))));
     return router;
   });
 };
