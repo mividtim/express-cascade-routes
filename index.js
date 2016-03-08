737b5318e8327650aad31cf72f240d30d5d681a7
Tim Garthwaite <tim@garthwaite.org>
2016-03-08 12:38:43 -0500

    Remove extraneous logging

--- index.js
+++ index.js
@@ -3,22 +3,15 @@ var fs = require('fs');
 
 module.exports = function(dirname) {
   var router = express.Router({ mergeParams: true });
-  console.log('======');
-  console.log(dirname);
-  console.log('------');
   fs.readdirSync(dirname).forEach(function(file) {
-    console.log(file);
     if(file === 'index.js'
         || file.substr(file.lastIndexOf('.') + 1) !== 'js'
         || file.substr(0, 1) === '_')
       return;
     routeName = '/' + file.substr(0, file.lastIndexOf('.'));
-    console.log(routeName);
     subRouter = require(dirname + routeName);
-    console.log(JSON.stringify(subRouter.stack[0].route));
     router.use(routeName, subRouter);
   });
-  console.log(router);
   return router;
 };
 
