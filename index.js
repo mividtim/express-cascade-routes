d2cf0ca5057c0e9d4265f7fd58224006b9624096
Tim Garthwaite <tim@garthwaite.org>
2016-03-08 13:25:14 -0500

    Handle subdirectories

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ var fs = require('fs');
 module.exports = function(dirname) {
   var router = express.Router({ mergeParams: true });
   fs.readdirSync(dirname).forEach(function(file) {
-    if(file === 'index.js'
-        || file.substr(file.lastIndexOf('.') + 1) !== 'js'
-        || file.substr(0, 1) === '_')
+    try {
+      fullPath = dirname + '/' + file;
+      var isDir = fs.lstatSync(fullPath).isDirectory();
+      if(isDir)
+        fullPath += '/' + 'index.js'
+      var routeName = '/' + (isDir ? file : file.substr(0, file.lastIndexOf('.')));
+      if(!fs.lstatSync(fullPath).isFile()
+          || file === 'index.js'
+          || !isDir && file.substr(file.lastIndexOf('.') + 1) !== 'js'
+          || file.substr(0, 1) === '_')
+        return;
+    }
+    catch(ex) {
+      console.log(ex);
       return;
-    routeName = '/' + file.substr(0, file.lastIndexOf('.'));
-    subRouter = require(dirname + routeName);
+    }
+    subRouter = require(fullPath);
     router.use(routeName, subRouter);
   });
   return router;
 };
-
